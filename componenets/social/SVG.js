9a486e3a987022018682c52086981478e6fe46a1
Charlie <charliebarger04@gmail.com>
2022-04-19 12:09:21 -0600

    Versioning untracked files

--- componenets/social/SVG.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { StlyedSocialLogo } from "./SocialLinksStyles";
-export const SVG = ({ children, ...rest }) => {
-  return <StlyedSocialLogo {...rest}>{children}</StlyedSocialLogo>;
-};
-
-export default SVG;
